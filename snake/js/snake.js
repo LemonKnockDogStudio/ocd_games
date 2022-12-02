28e515abfbdd9c11840fb8d07d7308cb3b4b3cc4
Siria <siria55lee@gmail.com>
2022-12-02 20:13:32 +0800

    minor update

--- snake/js/snake.js
+++ snake/js/snake.js
@@ -1,7 +1,7 @@
 import { getInputDirection } from "./input.js";
 
 
-export const SNAKE_SPEED = 10;  // X times per second
+export const SNAKE_SPEED = 2;  // X times per second
 const snakeBody = [
   {x: 11, y: 11}
 ];
